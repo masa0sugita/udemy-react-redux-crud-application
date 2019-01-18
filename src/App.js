4156182f49df85faadae79a5df5779a6376c7add
Masao SUGITA <masao.sugita@gmail.com>
2019-01-18 17:34:13 +0900

    関数コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+/* class App extends Component {
   render() {
     const text = 'これはてすとです';
     const dom = <h1 className="foo">{text}</h1>;
@@ -17,5 +17,26 @@ class App extends Component {
     );
   }
 }
+ */
 
+const App = () => {
+  const text = 'これはてすとです';
+  const dom = <h1 className="foo">{text}</h1>;
+  return (
+    <React.Fragment>
+      {dom}
+      <input
+        type="text"
+        onClick={() => {
+          console.log('Click!!');
+        }}
+      />
+    <Cat />
+    </React.Fragment>
+  );
+};
+
+const Cat = () => {
+  return <div>みゃー</div>
+}
 export default App;
