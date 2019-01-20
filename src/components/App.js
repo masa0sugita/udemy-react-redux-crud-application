42e863d653e957bb589b817221154af518cf1c25
Masao SUGITA <masao.sugita@gmail.com>
2019-01-20 22:02:54 +0900

    storeを使う設定をした

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  const text = 'これはてすとです';
+  const dom = <h1 className="foo">{text}</h1>;
+  return (
+    <React.Fragment>
+      {dom}
+      <Counter />
+    </React.Fragment>
+  );
+};
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  render() {
+    return (
+      <React.Fragment>
+      <div>counter: {this.state.count}</div>
+      <button onClick={this.handlePlusButton}>+1</button> | <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
