5db343c75a8d9a5e795491733071bb1e7372c208
Masao SUGITA <masao.sugita@gmail.com>
2019-01-20 19:41:21 +0900

    Reducer書いた

---
+++ src/reducers/count.js
@@ -0,0 +1,17 @@
+import { INCREMENT, DECREMENT } from '../actions'
+
+const initialState = { value: 0 }
+
+export default (state = initialState, action) => {
+    // eslint-disable-next-line default-case
+    switch (action.type ) {
+        case INCREMENT:
+            return { value: state.value + 1 }
+        
+        case DECREMENT:
+            return { value: state.value - 1 }
+
+        default:
+            return state
+    }
+}
