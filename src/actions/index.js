4b16ac5512cb9fecf0bb5a2c2f0b590e6ab9df11
Masao SUGITA <masao.sugita@gmail.com>
2019-01-22 17:02:58 +0900

    APIからの一覧データを取得してざっくり表示

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,24 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+import axios from "axios";
 
-export const increment = () => {
-    return {
-        type: INCREMENT
-    }
-}
+// export const INCREMENT = 'INCREMENT'
+// export const DECREMENT = 'DECREMENT'
 
-export const decrement = () => {
-    return {
-        type: DECREMENT
-    }
-}
+// export const increment = () => {
+//     return {
+//         type: INCREMENT
+//     }
+// }
 
+// export const decrement = () => {
+//     return {
+//         type: DECREMENT
+//     }
+// }
 
+export const READ_EVENTS = 'READ_EVENTS';
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
+const QUERYSTRING = '?token=token123';
+export const readEvents = () => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  dispatch({ type: READ_EVENTS, response });
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
-const store = createStore(reducer);
+
+import thunk from 'redux-thunk'
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
